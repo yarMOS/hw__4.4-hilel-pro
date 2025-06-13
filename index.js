19cc2c40a4e68f413b765c6ea1294998236ec0bc
Yaroslav Mospan <mossspan@gmail.com>
2025-06-13 13:31:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if (numOrStr === null) {
+//     console.log('ви скасували')
+// } else if (numOrStr.trim() === '') {
+//     console.log('Empty String');
+// } else if (isNaN(+numOrStr)) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
+
+switch (numOrStr) {
+    case numOrStr === null:
+        console.log('ви скасували');
+    break
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+    break
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
+    break
+    default:
+        console.log('OK!');
+}
